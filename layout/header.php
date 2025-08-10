f3152fac70d7b27c04839599d82e85b77336d313
meow <nina.nuss@fis-bw.de>
2025-08-10 15:55:33 +0200

    iii

--- layout/header.php
+++ layout/header.php
@@ -1,7 +1,7 @@
 <div id="header" style="background-color: white;display: flex;height: 13vh; padding-left: 0vh">
     <div style="display: flex; ">
         <img src="../images/bild.png" alt="bild"
-            style="margin-right: 60px;  margin-left: 5vh; margin-bottom: 2px; border-radius: 2vh;">
+            style="margin-right: 2.5vw;  margin-left: 2.5vw; margin-bottom: 2px; border-radius: 2vh;">
         <img src="../images/logo.png" alt="logo" style="padding-bottom: 25px;">
     </div>
     <link rel="stylesheet" href="../css/index_new.css">
@@ -33,14 +33,11 @@
         text-align: center;
     }
 
-    .parallelogram {
-        margin-right: 5px;
-    }
 </style>
 
-<div class="parallelogram col-md-12 ml-2">
+<div class="parallelogram col-md-12">
     <div class="para_inhalt d-flex text justify-content-between px-5">
-        <div class="clock"> Infoterminal Dashboard</div>
+        <div class="clock"> Infoterminal CJD Offenburg</div>
         <div class="clock">
             <div id="time"></div>
             <div>/</div>
