5ad58075242b04c112593dbef2ff39aef319d52a
Nina Nuss <maximnuss120697@gmail.com>
2025-08-12 16:21:14 +0200

    nnn

--- layout/sidebar.php
+++ layout/sidebar.php
@@ -1,10 +1,10 @@
     <div id="rowForCards" class="col-md-2 text-center">
-        <select id="selectorInfoterminalForCards" class="form-select" aria-label="Default select example">
+        <!-- <select id="selectorInfoterminalForCards" class="form-select" aria-label="Default select example">
             <option selected>Open this select menu</option>
             <option value="1">One</option>
             <option value="2">Two</option>
             <option value="3">Three</option>
-        </select>
+        </select> -->
 
 
         <small>bitte Infoseite auswählen: </small>
