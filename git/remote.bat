ad55fe642d9e94dc18fce2b190ab3df38ad3f32b
nina.nuss <nina.nuss01@gmail.com>
2025-08-14 09:42:06 +0200

    nnn

--- git/remote.bat
+++ git/remote.bat
@@ -1,8 +1,8 @@
-set REMOTE_URL="https://github.com/Nina-Nuss/infoterminal.git"
+cd ..
 
 git config --global user.name "nina.nuss"
 git config --global user.email "nina.nuss01@gmail.com"
 
 git init
 
-git remote add origin %REMOTE_URL%
+git remote add origin "https://github.com/Nina-Nuss/infoterminal.git"
